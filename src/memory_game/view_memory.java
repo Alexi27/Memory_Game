715da5b5cddfe74dad5d1444b3418acd0bd8ab40
Alexi27 <alexi27cuellar@hotmail.fr>
2018-04-07 11:19:52 +0100

    add button with picture

--- src/memory_game/view_memory.java
+++ src/memory_game/view_memory.java
@@ -3,10 +3,21 @@ package memory_game;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
+import java.awt.GridBagLayout;
+import javax.swing.JButton;
+import java.awt.GridBagConstraints;
+import java.awt.GridLayout;
+
+import javax.imageio.ImageIO;
+import javax.swing.ButtonGroup;
+import javax.swing.ImageIcon;
+import javax.swing.JPanel;
+import java.awt.Insets;
 
 public class view_memory {
 
 	private JFrame frame;
+	private final ButtonGroup buttonGroup = new ButtonGroup();
 
 	/**
 	 * Launch the application.
@@ -36,8 +47,22 @@ public class view_memory {
 	 */
 	private void initialize() {
 		frame = new JFrame();
-		frame.setBounds(100, 100, 450, 300);
+		frame.setBounds(100, 100, 700, 600);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		GridBagLayout gridBagLayout = new GridBagLayout();
+		gridBagLayout.columnWidths = new int[]{0, 0, 0, 0, 0};
+		gridBagLayout.rowHeights = new int[]{0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+		gridBagLayout.columnWeights = new double[]{0.0, 0.0, 0.0, 1.0, Double.MIN_VALUE};
+		gridBagLayout.rowWeights = new double[]{0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
+		frame.getContentPane().setLayout(gridBagLayout);
+		
+		JButton button = new JButton("");
+		button.setIcon(new ImageIcon("/home/alexi/eclipse-workspace/memory_game/src/car.png"));
+		buttonGroup.add(button);
+		GridBagConstraints gbc_button = new GridBagConstraints();
+		gbc_button.gridx = 3;
+		gbc_button.gridy = 8;
+		frame.getContentPane().add(button, gbc_button);
 	}
 
 }
