1c2383da799755724f8f67bd9106f0e952005e3c
Alexi27 <alexi27cuellar@hotmail.fr>
2018-04-07 10:39:17 +0200

    fisrt commit

---
+++ src/memory_game/view_memory.java
@@ -0,0 +1,43 @@
+package memory_game;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+
+public class view_memory {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					view_memory window = new view_memory();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public view_memory() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+}
